9f6524a11eba8b4cd48d3c3320e78b51f5bf25ec
Ringo84 <mar.montesi@gmail.com>
2022-06-16 16:21:15 +0200

    Changed input fields

--- src/App.js
+++ src/App.js
@@ -2,22 +2,24 @@ import React from "react";
 import { useForm } from "./useForm";
 
 const App = () => {
-  const [values, handleChange] = useForm({ email: "", password: "" });
-  // const [values2, handleChange2] = useForm({ firstName: "", lastName: "" });
+	const [values, handleChange] = useForm({ firstName: "", lastName: "" });
+	// const [values2, handleChange2] = useForm({ firstName: "", lastName: "" });
 
-  return (
-    <div>
-      <>
-        <input name="email" value={values.email} onChange={handleChange} />
-        <input
-          type="password"
-          name="password"
-          value={values.password}
-          onChange={handleChange}
-        />
-      </>
-    </div>
-  );
+	return (
+		<div>
+			<>
+				<input name="firstName" value={values.firstName} onChange={handleChange} />
+				<input name="lastName" value={values.lastName} onChange={handleChange} />
+				{values.firstName !== "" || values.lastName !== "" ? (
+					<div>
+						<span>
+							Hello {values.firstName} {values.lastName}!
+						</span>
+					</div>
+				) : null}
+			</>
+		</div>
+	);
 };
 
 export default App;
